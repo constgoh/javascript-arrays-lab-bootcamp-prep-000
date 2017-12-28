883df21ad24e6147fddb4031ee11d7b5292d1277
Constance Goh <constgoh@gmail.com>
2017-12-28 12:36:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-var kitten = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(kitten, name) {
   kitten.push(name)
   return kitten
